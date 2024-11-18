461a80b3ab6f0510022de80b2b3fafdc70846560
Evan Frische <FrischeEvan@gmail.com>
2024-11-18 12:05:59 -0700

    Add Loading Animation & Make Admin Pages NO CACHE

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,6 +1,9 @@
 import { ReactNode } from 'react';
 import { Nav, NavLink } from "@/components/Nav"
 
+// FORCES NEXT JS NOT TO CACHE ADMIN PAGES
+export const dynamic = 'force-dynamic'
+
 export default function AdminLayout({
   children,
 }: {
