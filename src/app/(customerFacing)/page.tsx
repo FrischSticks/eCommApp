713a4aefe7caa0ed56e4b96c754d45cf4616c585
Evan Frische <FrischeEvan@gmail.com>
2024-12-05 13:39:34 -0700

    Created ProductCard Component/Skeleton Loader  & Added to Home

--- src/app/(customerFacing)/page.tsx
+++ src/app/(customerFacing)/page.tsx
@@ -3,6 +3,8 @@ import { ArrowRight } from "lucide-react";
 import db from "@/db/prisma"; 
 import { Product } from "@prisma/client";
 import Link from "next/link"
+import { ProductCard, ProductCardSkeleton } from "@/components/ProductCard";
+import { Suspense } from "react";
 
 
 // Fetch the Top-Selling Products
@@ -24,41 +26,58 @@ async function getNewestProducts() {
 }
 
 export default function HomePage() {
-    return (
-      <main className="space-y-12">
-        <ProductGridSection
-          title="Most Popular"
-          productsFetcher={ getTopSellingProducts }
-        />
-        <ProductGridSection 
-          title="Newest" 
-          productsFetcher={ getNewestProducts } 
-        />
-      </main>
-    )
-  }
-  
-  type ProductGridSectionProps = {
-    title: string
-    productsFetcher: () => Promise<Product[]>
-  }
-  
-  function ProductGridSection({
-    productsFetcher,
-    title,
-  }: ProductGridSectionProps) {
-    return (
-      <div className="space-y-4">
-        <div className="flex gap-4">
-          <h2 className="text-3xl font-bold">{title}</h2>
-          <Button variant="outline" asChild>
-            <div className="space-x-2">
-              <Link href="/products">
-                <span>View All</span>
-              </Link>
-              <ArrowRight className="size-4" />
-            </div>
-          </Button>
-        </div>
+  return (
+    <main className="space-y-12">
+      <ProductGridSection
+        title="Most Popular"
+        productsFetcher={getTopSellingProducts}
+      />
+      <ProductGridSection title="Newest" productsFetcher={getNewestProducts} />
+    </main>
+  )
+}
+
+type ProductGridSectionProps = {
+  title: string
+  productsFetcher: () => Promise<Product[]>
+}
+
+function ProductGridSection({
+  productsFetcher,
+  title,
+}: ProductGridSectionProps) {
+  return (
+    <div className="space-y-4">
+      <div className="flex gap-4">
+        <h2 className="text-3xl font-bold">{title}</h2>
+        <Button variant="outline" asChild>
+          <Link href="/products" className="space-x-2">
+            <span>View All</span>
+            <ArrowRight className="size-4" />
+          </Link>
+        </Button>
       </div>
-)}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <Suspense
+          fallback={
+            <>
+              <ProductCardSkeleton />
+            </>
+          }
+        >
+          <ProductSuspense productsFetcher={productsFetcher} />
+        </Suspense>
+      </div>
+    </div>
+  )
+}
+
+async function ProductSuspense({
+  productsFetcher,
+}: {
+  productsFetcher: () => Promise<Product[]>
+}) {
+  return (await productsFetcher()).map(product => (
+    <ProductCard key={product.id} {...product} />
+  ))
+}
---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,61 @@
+
+import { Button } from "./ui/button";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card";
+import Image from "next/image";
+import { formatCurrency } from "@/lib/formatters";
+import Link from "next/link";
+
+type ProductCardProps = {
+    id: string
+    name: string
+    priceInCents: number
+    description: string
+    imagePath: string
+}
+
+export function ProductCardSkeleton() {
+    return (
+        <Card className="flex overflow-hidden flex-col animate-pulse">
+            <div className="w-full aspect-video bg-gray-300" />
+            <CardHeader>
+                <CardTitle>
+                    <div className="w-3/4 h-6 rounded-full bg-gray-300" />
+                </CardTitle>
+                <CardDescription>
+                    <div className="w-1/2 h-4 rounded-full bg-gray-300" />
+                </CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-2">
+                <div className="w-full h-4 rounded-full bg-gray-300" />
+                <div className="w-full h-4 rounded-full bg-gray-300" />
+                <div className="w-3/4 h-4 rounded-full bg-gray-300" />
+            </CardContent>
+            <CardFooter>
+                <Button className="w-full" size="lg" disabled></Button>
+            </CardFooter>
+        </Card>
+    )
+}
+
+export function ProductCard( { id, name, priceInCents, description, imagePath }:
+    ProductCardProps
+ ) {
+    return (
+        <Card className="flex overflow-hidden flex-col">
+            <div className="relative w-full h-auto aspect-video">
+                <Image src={imagePath} alt={name} fill />
+            </div>
+            <CardHeader>
+                <CardTitle> {name} </CardTitle>
+                <CardDescription> {formatCurrency(priceInCents / 100)} </CardDescription>
+            </CardHeader>
+            <CardContent className="flex-grow">
+                <p className="line-clamp-4"> {description} </p>
+            </CardContent>
+            <CardFooter>
+                <Button className="w-full" size="lg" asChild>
+                    <Link href={`/products/${id}/purchase`}> <span> Purchase </span> </Link>
+                </Button>
+            </CardFooter>
+        </Card>
+)}
