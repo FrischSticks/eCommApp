eb4480464c3452a39cc97c4222175d20e0a8931e
Evan Frische <FrischeEvan@gmail.com>
2024-12-07 16:07:51 -0700

    Find Unique Product by id  w/ Prisma

--- src/app/(customerFacing)/products/[id]/purchase/page.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/page.tsx
@@ -1,3 +1,15 @@
-export default function PurchasePage() {
-    return <h1>HELLO</h1>
+import db from "@/db/prisma"
+import { notFound } from "next/navigation"
+
+export default async function PurchasePage({ 
+    params: { id }
+} : {
+    params: { id: string}
+}) {
+    const product = await db.product.findUnique({ 
+        where: { id } })
+        if (product == null ) return notFound()
+
+
+    return <h1>{product.name}</h1>
 }
