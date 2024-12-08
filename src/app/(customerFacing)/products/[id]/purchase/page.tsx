a9535b41d3cd2530c853799f2938c23e6267773b
Evan Frische <FrischeEvan@gmail.com>
2024-12-08 14:19:00 -0700

    Begin Stripe Checkout Form

--- src/app/(customerFacing)/products/[id]/purchase/page.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/page.tsx
@@ -1,6 +1,7 @@
 import db from "@/db/prisma"
 import { notFound } from "next/navigation"
 import Stripe from "stripe"
+import { CheckoutForm } from "./_components/CheckoutForm"
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
 
@@ -22,5 +23,11 @@ export default async function PurchasePage({
         metadata: {productId: product.id}
     })
 
-    return <h1>{product.name}</h1>
+    // This is Used by the Client to Associate w/ paymentIntent Created Above (almost like an id)
+    if ( paymentIntent.client_secret == null ) {
+        throw Error("Stripe Failed to Create Payment Intent")
+    }
+
+    // Return as Separate Component, because there are a Lot of React Hooks (CSR)
+    return <CheckoutForm product={product} clientSecret={paymentIntent.client_secret} />
 }
