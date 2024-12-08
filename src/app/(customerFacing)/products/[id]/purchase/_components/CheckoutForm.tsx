80acd882de4226ec68e60928d2bc51f5ed117551
Evan Frische <FrischeEvan@gmail.com>
2024-12-08 15:30:30 -0700

    Checkout Form Created w/ Stripe

--- src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/_components/CheckoutForm.tsx
@@ -1,8 +1,26 @@
+"use client"
+
+import { Elements, PaymentElement, useElements, useStripe } from "@stripe/react-stripe-js"
+import { loadStripe } from "@stripe/stripe-js"
+
 type CheckoutFormProps = {
     product: {},
     clientSecret: string
 }
 
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string)
+
 export function CheckoutForm({ product, clientSecret } : CheckoutFormProps ) {
-    return <h1> Checkout Form </h1>
+    // Use Elements like a Conext Wrapper & Add Components
+    // Add appearance as option to Customize
+    return <Elements options={{clientSecret}} stripe={stripePromise}>
+        <Form />
+    </Elements>
+}
+
+function Form() {
+    const stripe = useStripe()
+    const elements = useElements()
+    
+    return <PaymentElement />
 }
