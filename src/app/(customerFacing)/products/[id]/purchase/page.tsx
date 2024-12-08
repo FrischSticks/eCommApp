64dc17ef5864e89f50e8ab7e6845b4778b717397
Evan Frische <FrischeEvan@gmail.com>
2024-12-08 14:05:08 -0700

    Stripe Setup & Create Payment Intent

--- src/app/(customerFacing)/products/[id]/purchase/page.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/page.tsx
@@ -1,15 +1,26 @@
 import db from "@/db/prisma"
 import { notFound } from "next/navigation"
+import Stripe from "stripe"
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
 
 export default async function PurchasePage({ 
     params: { id }
 } : {
     params: { id: string}
 }) {
+
     const product = await db.product.findUnique({ 
         where: { id } })
-        if (product == null ) return notFound()
-
+    if (product == null ) return notFound()
+    
+    // Create Stripe Payment Intent
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: product.priceInCents,
+        currency: "USD",
+        // Ties Purchase to Given Product
+        metadata: {productId: product.id}
+    })
 
     return <h1>{product.name}</h1>
 }
