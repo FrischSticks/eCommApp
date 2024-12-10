0240f73f328854a32b66624cdd9356c9d942f459
Evan Frische <FrischeEvan@gmail.com>
2024-12-10 15:57:49 -0700

    Begin Stripe Webhook

---
+++ src/app/webhooks/stripe/route.ts
@@ -0,0 +1,29 @@
+import db from "@/db/prisma";
+import { NextRequest, NextResponse } from "next/server";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_WEBHOOK_SECRET as string)
+
+// Called By Stripe - Gives Info from Body
+export async function POST(req: NextRequest) {
+    // constructEvent() Verifies that everything being passed is from Stripe
+    const event = await stripe.webhooks.constructEvent( await 
+        req.text(), 
+        req.headers.get("stripe-signature") as string,
+        process.env.STRIPE_WEBHOOK_SECRET as string
+    )
+
+    // Called Every Successful Charge on Stripe
+    if (event.type === "charge.succeeded") {
+        // Customer Charge
+        const charge = event.data.object
+
+        const productId = charge.metadata.productId
+        const email = charge.billing_details.email
+        const priceInCents = charge.amount
+
+        // Ensure Product Exists
+        const product = await db.product.findUnique({ where: { id: productId}})
+        if ( product == null || email == null ) return new NextResponse("Bad Request", {status: 400})
+    }
+}
